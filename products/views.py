9a62be460816bd0fdcd97437c7dbb11401f9b82b
Arya Vats <aryavats123@gmail.com>
2024-03-18 21:03:02 +0530

    finish product

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+import random
 
-from .models import Product
+from rest_framework.views import APIView
+
+from .models import Product, User
 from .serializers import ProductSerializer
 
 
@@ -23,7 +26,22 @@ class ProductViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def update(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
-        pass
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            "id": user.id
+        })
